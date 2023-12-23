443f2890f6072a3b633bd95373cb8c18a2a98f0f
achim <wowcata0050@gmail.com>
2023-12-23 20:36:58 +0100

    variablen extern Save veränderte Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 // Hier können die Variablen und Listen für das Spiel definiert werden
 
-var RassenWahl = mutableListOf(
+var rassenWahl = mutableListOf(
         "Menschen der alten Welt" ,
         " Elbenvolk der Lumira " ,
         "Zwerge der Eisengrube ",
@@ -11,6 +11,13 @@ var klassenWahlMenschen = mutableListOf("Krieger, Magier, Königsritter, Pendler
 var klassenWahlLumira = mutableListOf("Waldhüter, Weiser der Ätherie, Naturbändiger, Sternenweiser")
 var weaponMutableList = mutableListOf("..")
 val ausgewaehlteRasse: String = ""
+val spielerName = readln() ?: "Spieler"
+val spieler = Spieler(spielerName)
+val boss = "Jake Michaelson"
+
+
+
+
 //val indexDesGewaehltenEintrags: String = readln()
 
 // Spielerklasse, um die Spielerdaten zu speichern
@@ -27,38 +34,21 @@ data class Spieler(
     // Weitere Spielerattribute können hinzugefügt werden
 )
 
+data class jakeMichaelson1(
+    var name: String = "Jake Michaelson",
+    var level: Int = 30,
+    var erfahrung: Int = 25000
+
+)
+
+
+
 // Funktion zum Starten des Spiels
-fun startGame() {
-    println("Willkommen im mittelalterlichen Königreich! Ein Abenteuer erwartet dich.")
-
-    // Spielererstellung und Einführung
-    println("Welche Rasse willst du Spielen? ")
-    println("folgende Rassen stehen zur auswahl: ${RassenWahl.joinToString()}")
-    val ausgewaehlteRasse: String = ""
-    val indexDesGewaehltenEintrags: String = readln()
-
-    println("du bist ein $indexDesGewaehltenEintrags")
-
-
-    println("Wie ist dein Name, tapferer Recke?")
-    val spielerName = readln() ?: "Spieler"
-    val spieler = Spieler(spielerName)
-
-    println("Hallo, $spielerName! Begib dich auf deine Reise und sei der Held der Geschichte.")
-
-
-}
-
-
-fun prolog(){
-
-    println("01.01.1634")
-    println("Die geschichte beginnt ihren Lauf in Silberbach einem kleinen dorf Südlich von Falkenhorst und dem derzeit Herrschenden König Henry von Lancaster ")
-
-
-}
 
 fun main() {
+
+
+
     println("Willkommen im Hauptmenü")
     println("1. Neues Spiel")
     println("2. Spiel Laden")
@@ -83,4 +73,55 @@ fun main() {
 
 
 
+
 }
+
+
+fun startGame() {
+    println("Willkommen im mittelalterlichen Königreich! Ein Abenteuer erwartet dich.")
+
+    // Spielererstellung und Einführung
+    println("Welche Rasse willst du Spielen? ")
+    println("folgende Rassen stehen zur auswahl: ${rassenWahl.joinToString()}")
+    rassenWahl.forEachIndexed { index, rasse -> println("${index + 1}. $rasse") }
+
+    print("Bitte gib den Namen der gewählten Rasse ein: ")
+    val eingabe = readln()?.trim()
+
+    val gewaehlteRasse = rassenWahl.find { it.equals(eingabe, ignoreCase = true) }
+
+    if (gewaehlteRasse != null) {
+        println("Du hast die Rasse '$gewaehlteRasse' gewählt. Viel Spaß beim Abenteuer!")
+        // Hier kannst du weitere Logik für das Spiel mit der gewählten Rasse implementieren
+    } else {
+        println("Ungültige Auswahl. Bitte gib den Namen einer verfügbaren Rasse ein.")
+    }
+
+    println("Wie ist dein Name, tapferer Recke?")
+    spielerName
+
+
+
+
+
+    println("Hallo, $spielerName! Begib dich auf deine Reise und sei der Held der Geschichte.")
+    prolog()
+
+
+}
+fun prolog(){
+
+    println("01.01.1634")
+    println("Die geschichte beginnt ihren Lauf in Silberbach einem kleinen dorf Südlich von Falkenhorst und dem derzeit Herrschenden König Henry von Lancaster ")
+    println("Der Junge $spielerName erwacht aus seinem Kinderbett, und sieht überall Blut")
+    println("plötzlich sieht er $boss mit dem Rücken zum Kinderbett, lachend ein Schwert in der Hand")
+    println("die andere Hand richtung Wand gestreckt, und an dessen ende MAAAMAAA!!!!!")
+    println("Jake dreht sich um sieht den Knaben lacht und sagt mit furchtergender ruhe in seiner Stimme")
+    println("Sie es dir gut an junge, nicht lange und dich ereilt dass selbe Schicksal, aber bis dahin sollst du leiden")
+    println("Solange leiden wie ich nach diesem Schwert suchen musste")
+
+
+
+}
+
+
--- src/main/kotlin/variables.kt
+++ src/main/kotlin/variables.kt
@@ -1,3 +1,5 @@
+package variable
+
 object meineVariablen {
 
 
