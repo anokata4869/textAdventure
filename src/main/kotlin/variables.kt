e2f50d1b023acb6782f15ad39ef3fed3633a34e4
achim <wowcata0050@gmail.com>
2023-12-23 20:22:58 +0100

    variablen extern Save

---
+++ src/main/kotlin/variables.kt
@@ -0,0 +1,19 @@
+object meineVariablen {
+
+
+    var rassenWahl = mutableListOf(
+        "Menschen der alten Welt",
+        " Elbenvolk der Lumira ",
+        "Zwerge der Eisengrube ",
+        "Feen des Lichtwaldes",
+        "Nymphen des Wasserbergs"
+    )
+
+    var klassenWahlMenschen = mutableListOf("Krieger, Magier, Königsritter, Pendler")
+    var klassenWahlLumira = mutableListOf("Waldhüter, Weiser der Ätherie, Naturbändiger, Sternenweiser")
+    var weaponMutableList = mutableListOf("..")
+    val ausgewaehlteRasse: String = ""
+
+    val boss = "Jake Michaelson"
+
+}
