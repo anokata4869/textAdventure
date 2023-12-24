0382d67f1bcdefe16630101be83c00a81a051dc8
achim <wowcata0050@gmail.com>
2023-12-24 12:19:40 +0100

    variablen extern Save veränderte Main weitere listen (Waffen, ebenen, Level+erfahrung) geschriebener Prolog

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,21 @@
-// Hier können die Variablen und Listen für das Spiel definiert werden
+// ausführung der Listen fürs Spiel
+
+val levelEbenen = mutableListOf(
+        "Schwarzdornwald der Schatten\n" ,
+        "Rittertum der Verlorenen Tugenden\n" ,
+        "Zauberreich der Vergessenen Magie\n" ,
+        "Nebelmoor der Versunkenen Seelen\n" ,
+        "Drachenklaue-Tal der Legenden\n" ,
+        "Burgmauern des Ewigen Zwists\n" ,
+        "Geisterpfade der Verbannten\n" ,
+        "Eichenherz-Wald der Geheimnisse\n" ,
+        "Sturmwolken-Gebirge des Unheils\n" ,
+        "Sumpfland der Finsteren Hexerei\n" ,
+        "Königsgruft der Vergangenen Ära\n" ,
+        "Dämmerhöhen der Unendlichen Einsamkeit\n" ,
+        "Weg der Ewigen Prüfungen\n" ,
+        "Verborgenes Tal der Mystischen Artefakte\n" ,
+        "Raubritter-Ruinen des Bösen Erbes")
 
 var rassenWahl = mutableListOf(
         "Menschen der alten Welt" ,
@@ -8,17 +25,81 @@ var rassenWahl = mutableListOf(
         "Nymphen des Wasserbergs")
 
 var klassenWahlMenschen = mutableListOf("Krieger, Magier, Königsritter, Pendler")
+
 var klassenWahlLumira = mutableListOf("Waldhüter, Weiser der Ätherie, Naturbändiger, Sternenweiser")
-var weaponMutableList = mutableListOf("..")
+
+var waffenSchwert = mutableListOf(
+        "Excalibur\n" ,
+        "Durandal\n" ,
+        "Joyeuse\n" ,
+        "Gram\n" ,
+        "Clarent\n" ,
+        "Kusanagi-no-Tsurugi\n" ,
+        "Zulfiqar\n" ,
+        "Tizona\n" ,
+        "Shamshir-e Zomorrodnegar\n" ,
+        "Durendal")
+
+var waffenAexte = mutableListOf(
+        "Labrys\n" ,
+        "Dane Axe\n" ,
+        "Francisca\n" ,
+        "Throwing Axe\n" ,
+        "Viking Bearded Axe\n" ,
+        "Bipennis\n" ,
+        "Tomahawk\n" ,
+        "Bardiche\n" ,
+        "Hache du Combat\n" ,
+        "Cholera Axe")
+
+var waffenBoegen = mutableListOf(
+        "Artemis Bow\n" ,
+        "Longbow of Robin Hood\n" ,
+        "Mongol Bow\n" ,
+        "Yumi\n" ,
+        "English Longbow\n" ,
+        "Recurve Bow\n" ,
+        "Compound Bow\n" ,
+        "Crossbow of William Tell\n" ,
+        "Turkish Bow\n" ,
+        "Horn Bow")
+
+var waffenSpeere = mutableListOf(
+        "Gungnir\n" ,
+        "Lance of Longinus\n" ,
+        "Pilum\n" ,
+        "Assegai\n" ,
+        "Boar Spear\n" ,
+        "Ranseur\n" ,
+        "Partisan\n" ,
+        "Pike\n" ,
+        "Trident\n" ,
+        "Javelin")
+
+var waffenMagerStaebe = mutableListOf(
+        "Elder Wand\n" ,
+        "Scepter of Ra\n" ,
+        "Caduceus\n" ,
+        "Crystal Wand\n" ,
+        "Staff of Gandalf\n" ,
+        "Phoenix Feather Wand\n" ,
+        "Rod of Asclepius\n" ,
+        "Arcane Staff\n" ,
+        "Divining Rod\n" ,
+        "Wizard's Staff")
+
+val levelListe = (1..70).toList()
+
+// Variablen Deklarierung
+
 val ausgewaehlteRasse: String = ""
 val spielerName = readln() ?: "Spieler"
 val spieler = Spieler(spielerName)
 val boss = "Jake Michaelson"
 
+val basisErfahrung = 1000 // Erfahrungspunkte für Level 1
+val endErfahrung = 190000 // Erfahrungspunkte für Level 70
 
-
-
-//val indexDesGewaehltenEintrags: String = readln()
 
 // Spielerklasse, um die Spielerdaten zu speichern
 data class Spieler(
@@ -29,21 +110,38 @@ data class Spieler(
     var waffen: MutableList<String> = mutableListOf(),
 
 
-
-
     // Weitere Spielerattribute können hinzugefügt werden
 )
-
+// gegenSpieler klasse
 data class jakeMichaelson1(
     var name: String = "Jake Michaelson",
     var level: Int = 30,
-    var erfahrung: Int = 25000
+    var erfahrung: Int = 84000
+    var skills: MutableList<String> = mutableListOf(),
+    var waffen: MutableList<String> = mutableListOf(),
 
 )
 
 
-
 // Funktion zum Starten des Spiels
+fun neuesSpiel() {
+    println("Neues Spiel wird gestartet...")
+    // Hier füge den Code hinzu, um ein neues Spiel zu starten
+}
+
+fun SpielLaden() {
+    println("Spiel wird geladen...")
+    // Hier füge den Code hinzu, um das Spiel zu laden
+}
+
+//fun einstellungen() {
+   // println("Einstellungen werden geöffnet...")
+    // Hier füge den Code hinzu, um die Einstellungen anzuzeigen und zu ändern
+//}
+//fun spielBeenden() {
+//  println("Spiel wird beendet")
+    // Hier füge den Code hinzu, um das Spiel zu beenden
+//}
 
 fun main() {
 
@@ -65,13 +163,6 @@ fun main() {
         4 -> spielBeenden()
         else -> println("Ungültige Option. Bitte wähle eine Zahl zwischen 1 und 4.")
     }
-
-
-
-
-
-
-
 
 
 }
@@ -102,8 +193,6 @@ fun startGame() {
 
 
 
-
-
     println("Hallo, $spielerName! Begib dich auf deine Reise und sei der Held der Geschichte.")
     prolog()
 
@@ -120,8 +209,4 @@ fun prolog(){
     println("Sie es dir gut an junge, nicht lange und dich ereilt dass selbe Schicksal, aber bis dahin sollst du leiden")
     println("Solange leiden wie ich nach diesem Schwert suchen musste")
 
-
-
 }
-
-
